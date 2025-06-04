18246a13f55d951ba10ec6fcf91eedced1d27a48
kxboon <boonkx8888@gmail.com>
2025-06-04 12:09:36 +0800

    Second commit: added organizer, event, and ticket tables

--- db_schema.sql
+++ db_schema.sql
@@ -6,6 +6,7 @@ BEGIN TRANSACTION;
 
 -- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
 
+--organizers table
 CREATE TABLE IF NOT EXISTS organizers(
     organizer_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
@@ -15,24 +16,37 @@ CREATE TABLE IF NOT EXISTS organizers(
     site_description TEXT DEFAULT 'Welcome to my event site',
 )
 
-CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+--events table
+CREATE TABLE IF NOT EXISTS events(
+    event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    date TEXT NOT NULL,
+    location TEXT NOT NULL,
+    state TEXT Default 'planning',
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    published_at TEXT,
+    organizer_id INTEGER NOT NULL,
+    FOREIGN KEY (organizer_id) REFERENCES organizers(organizer_id) ON DELETE CASCADE
+);
+
+--Ticket table
+CREATE TABLE IF NOT EXISTS tickets(
+    ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    price REAL NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE
 );
 
 -- Insert default data (if necessary here)
 
 -- Set up three users
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
-
--- Give Simon two email addresses and Diane one, but Harry has none
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+INSERT INTO organizers (name, email, password, site_name, site_description) VALUES
+('Simon Star', 'simon@example.com', 'hashedpassword1', 'Simon Events', 'Awesome yoga events'),
+('Dianne Dean', 'dianne@example.com', 'hashedpassword2', 'Dianne Yoga', 'Yoga classes for all ages'),
+('Harry Hilbert', 'harry@example.com', 'hashedpassword3', 'Harry\'s Events', 'Fun and engaging events');
 
 COMMIT;
 
